7c23dfad257105cae67c695fd61d15f4664b02ed
CtripaClip <82753111+CtripaClip@users.noreply.github.com>
2023-07-01 21:03:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+from requests import get
 
+api = 'TOCEN' # your API tocen
+lat = 42.56955
+lon = 47.86447
+url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}'
+
+def request(url): # requests
+    req = requests.get(url).json()
+    print(req)
+
+request(url) # call function
