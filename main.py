14bcc417e0899b5e04882f10b80ba129482446dd
CtripaClip <82753111+CtripaClip@users.noreply.github.com>
2023-07-02 17:55:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
 from requests import get
 
-api = 'TOCEN' # your API tocen
+api = 'TOCEN' # settings
 lat = 42.56955
 lon = 47.86447
-url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}'
+
+city = "Armavir"
+
+url = f'https://api.openweathermap.org/data/2.5/weather?name={city}&appid={api}' # links
+url2 = f'https://pro.openweathermap.org/data/2.5/forecast/climate?lat={lat}&lon={lon}&appid={api}'
 
 def request(url): # requests
-    req = requests.get(url).json()
-    print(req)
 
-request(url) # call function
+    def weather_now():
+        req = requests.get(url).json()
+        print(req)
+
+    def weather30():
+        req2 = requests.get(url2).json()
+        print(req2)
+
+if __name__ == '__main__':
+    request(url).weath # call function
